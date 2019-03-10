c08be172f488c16bb97bc68344622f4fc4c5aacb
kitasuke <yusuke2759@gmail.com>
2019-03-10 12:09:36 +0900

    Implement step

--- main.go
+++ main.go
@@ -3,29 +3,152 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/kitasuke/go-swift/swift"
+	"github.com/kitasuke/go-swift/utility"
 )
 
-func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+const (
+	BuildPathEnvKey   = "build_path"
+	IsSkipBuildEnvKey = "is_skip_build"
+	IsParallelEnvKey  = "is_parallel"
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+	BitriseSwiftTestResultKey = "BITRISE_SWIFT_TEST_RESULT"
+)
+
+// ConfigModel ...
+type ConfigModel struct {
+	// Project Parameters
+	BuildPath string
+
+	// Test Run Configs
+	isSkipBuild string
+	isParallel  string
+}
+
+func (configs ConfigModel) print() {
+	fmt.Println()
+
+	log.Infof("Project Parameters:")
+	log.Printf("- BuildPath: %s", configs.BuildPath)
+
+	fmt.Println()
+	log.Infof("Test Run Configs:")
+	log.Printf("- IsSkipBuild: %s", configs.isSkipBuild)
+	log.Printf("- IsParallel: %s", configs.isParallel)
+}
+
+func createConfigsModelFromEnvs() ConfigModel {
+	return ConfigModel{
+		// Project Parameters
+		BuildPath: os.Getenv(BuildPathEnvKey),
+
+		// Test Run Configs
+		isSkipBuild: os.Getenv(IsSkipBuildEnvKey),
+		isParallel:  os.Getenv(IsParallelEnvKey),
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
+}
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+func (configs ConfigModel) validate() error {
+	if err := validateRequiredInputWithOptions(configs.isSkipBuild, IsSkipBuildEnvKey, []string{"yes", "no"}); err != nil {
+		return err
+	}
+
+	if err := validateRequiredInputWithOptions(configs.isParallel, IsParallelEnvKey, []string{"yes", "no"}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//--------------------
+// Functions
+//--------------------
+
+func validateRequiredInput(value, key string) error {
+	if value == "" {
+		return fmt.Errorf("Missing required input: %s", key)
+	}
+	return nil
+}
+
+func validateRequiredInputWithOptions(value, key string, options []string) error {
+	if err := validateRequiredInput(value, key); err != nil {
+		return err
+	}
+
+	found := false
+	for _, option := range options {
+		if option == value {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("Invalid input: (%s) value: (%s), valid options: %s", key, value, strings.Join(options, ", "))
+	}
+
+	return nil
+}
+
+// ExportEnvironmentWithEnvman ...
+func ExportEnvironmentWithEnvman(keyStr, valueStr string) error {
+	return command.New("envman", "add", "--key", keyStr).SetStdin(strings.NewReader(valueStr)).Run()
+}
+
+func exportTestResult(status string) {
+	if err := ExportEnvironmentWithEnvman(BitriseSwiftTestResultKey, status); err != nil {
+		log.Warnf("Failed to export: %s, error: %s", BitriseSwiftTestResultKey, err)
+	}
+}
+
+func failf(format string, v ...interface{}) {
+	exportTestResult("failed")
+	log.Errorf(format, v...)
+	os.Exit(1)
+}
+
+//--------------------
+// Main
+//--------------------
+
+func main() {
+	configs := createConfigsModelFromEnvs()
+	configs.print()
+	if err := configs.validate(); err != nil {
+		failf("Issue with input: %s", err)
+	}
+
+	fmt.Println()
+	log.Infof("Other Configs:")
+
+	isSkipBuild := configs.isSkipBuild == "yes"
+	isParallel := configs.isParallel == "yes"
+
+	swiftVersion, err := utility.GetSwiftVersion()
+	if err != nil {
+		failf("Failed to get the version of swift! Error: %s", err)
+	}
+
+	log.Printf("* swift_version: %s (%s)", swiftVersion.Version, swiftVersion.Target)
+
+	fmt.Println()
+
+	// setup CommandModel for test
+	testCommandModel := swift.NewTestCommand()
+	testCommandModel.SetBuildPath(configs.BuildPath)
+	testCommandModel.SetSkipBuild(isSkipBuild)
+	testCommandModel.SetIsParallel(isParallel)
+
+	log.Infof("$ %s\n", testCommandModel.PrintableCmd())
+
+	if err := testCommandModel.Run(); err != nil {
+		failf("Test failed, error: %s", err)
+	}
+
+	exportTestResult("succeeded")
 }
